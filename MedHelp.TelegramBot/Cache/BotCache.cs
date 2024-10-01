06220f758a5c446b8bfb832ef153eee8adb5902f
Dan <monreal007@gmail.com>
2024-10-01 21:46:07 +0400

    Add TelegramBot

---
+++ MedHelp.TelegramBot/Cache/BotCache.cs
@@ -0,0 +1,32 @@
+﻿using MedHelp.Core.Models;
+using PRTelegramBot.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedHelp.TelegramBot.Cache
+{
+  /// <summary>
+  /// Кеш для пошагового выполнения команд бота
+  /// </summary>
+  public class BotCache : ITelegramCache
+  {
+    public long Id { get; set; }
+    public string DoctorName { get; set; }
+    public List<Disease> Diseases {  get; set; } 
+    public List<Drug> Drugs { get; set; }
+    public string OtherData { get; set; }
+
+    public bool ClearData()
+    {
+      Id = 0;
+      OtherData = string.Empty;
+      Diseases = new List<Disease>();
+      DoctorName = string.Empty;
+      Drugs = new List<Drug>(); 
+      return true;
+    }
+  }
+}
---
+++ MedHelp.TelegramBot/Program.cs
@@ -0,0 +1,63 @@
+﻿using MedHelp.Core.Abstractions;
+using MedHelp.Core.Services;
+using MedHelp.DBase;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PRTelegramBot.Core;
+using PRTelegramBot.Extensions;
+using PRTelegramBot.Models.EventsArgs;
+
+namespace MedHelp
+{
+  internal class Program
+  {
+    static async Task Main(string[] args)
+    {
+      IConfigurationRoot config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+      string botToken = config.GetValue<string>("Telegram:Token");
+
+      var builder = WebApplication.CreateBuilder(args);
+
+      builder.Services.AddScopedBotHandlers();
+      builder.Services.AddLogging();
+      builder.Services.AddScoped<IDiseaseRepository, DiseaseRepository>();
+      builder.Services.AddScoped<IDrugRepository, DrugRepository>();
+      builder.Services.AddScoped<IUserRepository, UserRepository>();
+      builder.Services.AddScoped<IDrugService, DrugService>();
+      builder.Services.AddScoped<IUserService, UserService>();
+      builder.Services.AddScoped<DiseaseService>();
+      builder.Services.AddScoped<CreatePDFService>();
+
+      builder.Services.AddDbContext<AppDbContext>(options =>
+      options.UseNpgsql(
+        builder.Configuration.GetConnectionString("ConnectionString")));
+
+      var app = builder.Build();
+
+      var serviceProvider = app.Services.GetService<IServiceProvider>();
+
+      var telegram = new PRBotBuilder(botToken)
+                          .SetServiceProvider(serviceProvider)
+                          .Build();
+
+      telegram.Events.OnCommonLog += Telegram_OnLogCommon;
+      telegram.Events.OnErrorLog += Telegram_OnLogError;
+
+      await telegram.Start();
+
+      app.Run();
+
+      async Task Telegram_OnLogError(ErrorLogEventArgs e)
+      {
+        Console.WriteLine(e.Exception.Message);
+      }
+
+      async Task Telegram_OnLogCommon(CommonLogEventArgs e)
+      {
+        Console.WriteLine($"{e.Message}");
+      }
+    }
+  }
+}
